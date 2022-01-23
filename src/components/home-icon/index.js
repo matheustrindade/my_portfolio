0574da52790476f7fa15b5757b96e5347ae60899
MatheusTSantos <matheus.santos@pulsecrm.com.br>
2022-01-23 20:36:47 -0300

    alteração dos estilos do ícone da home para padrão BEM

--- src/components/home-icon/index.js
+++ src/components/home-icon/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import HomeStyles from './home-icon.module.css'
 
 const HomeIcon = () => {
-    return <div className={HomeStyles.container}>
-        <div className={HomeStyles.roof} />
-        <div className={HomeStyles.wall} />
+    return <div className={HomeStyles['home-icon']}>
+        <div className={HomeStyles['home-icon__roof']} />
+        <div className={HomeStyles['home-icon__wall']} />
     </div>
 }
 
